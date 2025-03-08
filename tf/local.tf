5957638844e1cd2dfb049b96b9b108dcc594a5d2
Jan Wächter <jan.waechter.54857@gmail.com>
2025-03-08 21:53:30 +0100

    Typo

--- tf/local.tf
+++ tf/local.tf
@@ -3,7 +3,7 @@ locals {
 
   container_image = "ghcr.io/open-webui/open-webui:main"
   container_cpu = "1"
-  container_memory = "2.0Gi"
+  container_memory = "2Gi"
 
   container_name = "openwebui"
 }
