931cb6cd0cc649b42caf4cf582e68c276a5ceaa2
Jan Wächter <jan.waechter.54857@gmail.com>
2025-03-08 21:00:00 +0100

    Increasing container size

--- tf/local.tf
+++ tf/local.tf
@@ -2,8 +2,8 @@ locals {
   location = "Switzerland North"
 
   container_image = "ghcr.io/open-webui/open-webui:main"
-  container_cpu = "0.5"
-  container_memory = "1.0Gi"
+  container_cpu = "1"
+  container_memory = "2.0Gi"
 
   container_name = "openwebui"
 }
