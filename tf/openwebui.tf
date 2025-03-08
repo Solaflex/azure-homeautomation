3c2f5aa556fcf85b471168a3729445371122b84c
Jan Wächter <jan.waechter.54857@gmail.com>
2025-03-08 17:19:31 +0100

    retention

--- tf/openwebui.tf
+++ tf/openwebui.tf
@@ -24,7 +24,7 @@ resource "azurerm_log_analytics_workspace" "openwebui" {
   location            = azurerm_resource_group.openwebui.location
   resource_group_name = azurerm_resource_group.openwebui.name
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+  retention_in_days   = 30
 }
 
 resource "azurerm_container_app_environment" "openwebui" {
