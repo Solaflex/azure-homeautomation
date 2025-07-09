4cabe7e935b978ebcbbccd044a7ce5c114aebad6
Jan Wächter <jan.waechter.54857@gmail.com>
2025-07-09 22:44:01 +0200

    acs

---
+++ tf/acs.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "acs" {
+  name     = "rg-azurecommunicationservice"
+  location = "West Europe"
+}
+
+resource "azurerm_email_communication_service" "main" {
+  name                = "acs-main"
+  resource_group_name = azurerm_resource_group.acs.name
+  data_location       = "Europe"
+}
+
+resource "azurerm_email_communication_service_domain" "zzl" {
+  name              = "zzl_com"
+  email_service_id  = azurerm_email_communication_service.main.id
+  domain_management = "CustomerManaged"
+}
+
+resource "azurerm_email_communication_service_domain_sender_username" "noreply" {
+  name                    = "noreply-su"
+  email_service_domain_id = azurerm_email_communication_service_domain.example.id
+}
