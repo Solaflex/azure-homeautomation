ebc769520aaea88fae4db118ad25794895cc744b
Jan Wächter <jan.waechter.54857@gmail.com>
2025-07-09 23:30:19 +0200

    service

--- tf/acs.tf
+++ tf/acs.tf
@@ -1,25 +1,3 @@
-resource "azurerm_resource_group" "acs" {
-  name     = "rg-azurecommunicationservice"
-  location = "West Europe"
-}
-
-resource "azurerm_email_communication_service" "main" {
-  name                = "acs-main"
-  resource_group_name = azurerm_resource_group.acs.name
-  data_location       = "Europe"
-}
-
-resource "azurerm_email_communication_service_domain" "zzl" {
-  name              = "zwischen-zwei-laendern.com"
-  email_service_id  = azurerm_email_communication_service.main.id
-  domain_management = "CustomerManaged"
-}
-
-resource "azurerm_email_communication_service_domain_sender_username" "noreply" {
-  name                    = "noreply"
-  email_service_domain_id = azurerm_email_communication_service_domain.zzl.id
-}
-
 # Create an application
 resource "azuread_application_registration" "smtp" {
   display_name = "smtp"
@@ -41,8 +19,43 @@ output "smtp_client_secret" {
 }
 # terraform output smtp_client_secret
 
+
+resource "azurerm_resource_group" "acs" {
+  name     = "rg-azurecommunicationservice"
+  location = "West Europe"
+}
+
+resource "azurerm_communication_service" "main" {
+  name                = "acs-main"
+  resource_group_name = azurerm_resource_group.acs.name
+  data_location       = "Europe"
+}
+
+resource "azurerm_email_communication_service" "main" {
+  name                = "acs-email-main"
+  resource_group_name = azurerm_resource_group.acs.name
+  data_location       = "Europe"
+}
+
+resource "azurerm_email_communication_service_domain" "zzl" {
+  name              = "zwischen-zwei-laendern.com"
+  email_service_id  = azurerm_email_communication_service.main.id
+  domain_management = "CustomerManaged"
+}
+
+resource "azurerm_email_communication_service_domain_sender_username" "noreply" {
+  name                    = "noreply"
+  email_service_domain_id = azurerm_email_communication_service_domain.zzl.id
+}
+
+resource "azurerm_communication_service_email_domain_association" "smtp" {
+  communication_service_id = azurerm_communication_service.main.id
+  email_service_domain_id  = azurerm_email_communication_service_domain.zzl.id
+}
+
+
 resource "azurerm_role_assignment" "smtp" {
   scope                = azurerm_resource_group.acs.id
   role_definition_name = "Communication and Email Service Owner"
-  principal_id         = azuread_service_principal.smtp.client_id
+  principal_id         = azuread_service_principal.smtp.object_id
 }
